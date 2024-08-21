7779e4d325a75e2e89b00b1597ac2d29338e6117
kemo <kemo.jallow@hannlync.com>
2024-08-21 17:34:08 +0800

    send email with document

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Sidebar from "../components/Sidebar";
-import AuthNavbar from "../components/AuthNavbar";
+import Sidebar from "@/components/Sidebar";
+import AuthNavbar from "@/components/AuthNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
